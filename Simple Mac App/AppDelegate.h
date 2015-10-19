d61e662ee591a4667552a21334d72356a181b937
fernandokarnagi <fernando.karnagi@ctc-g.com.sg>
2015-10-19 13:37:57 +0800

    New files

---
+++ Simple Mac App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Simple Mac App
+//
+//  Created by Fernando Karnagi on 19/10/15.
+//  Copyright © 2015 CTC Global. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Simple Mac App/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Simple Mac App
+//
+//  Created by Fernando Karnagi on 19/10/15.
+//  Copyright © 2015 CTC Global. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
